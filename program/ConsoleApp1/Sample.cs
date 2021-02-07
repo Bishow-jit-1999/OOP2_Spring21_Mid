50767419ed12eec3c4e06c51e8fdd931b293e9d6
Bishow-jit-1999 <bishowjitsaha.bs24@gmail.com>
2021-02-07 11:41:56 +0600

    Task 1 and Task 2

---
+++ program/ConsoleApp1/Sample.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Sample
+    {
+        int x;
+        int y;
+        internal void ShowDetails()
+        {
+            Console.WriteLine("X={0} \nY={1}", x, y);
+        }
+    }
+}
